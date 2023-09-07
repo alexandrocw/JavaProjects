86ea29c9bd2ac43c2dfa509468d77b8e2e45b3d3
Alexandro C.W <alexandrowijaya.aw2@gmail.com>
2023-09-07 23:29:46 +0700

    Trying spring boot demo

--- Testing/Main.java
+++ Testing/Main.java
@@ -1,9 +1,7 @@
 package Testing;
 public class Main {
   public static void main(String[] args) {
-    float f1 = 35e3f;
-    double d1 = 12E4d;
-    System.out.println(f1);
-    System.out.println(d1);
+    String[] cars = {"Toyota", "Mitsubishi", "Daihatsu"};
+    System.out.println(cars[0].length());
   }
 }
